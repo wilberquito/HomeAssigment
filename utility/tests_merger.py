4eff8a2f2f98f10d1dc4e437e443f797758e7db6
wilberquito <wbq.software@gmail.com>
2023-06-28 19:59:31 +0200

    fix coding problems

---
+++ utility/tests_merger.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+
+
+def merge(directory):
+    dfs = []
+    # Iterate over the files in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith('.csv'):  # Check if the file is a CSV file
+            file_path = os.path.join(directory, filename)
+            df = pd.read_csv(file_path)
+            dfs.append(df)
+
+    # Concatenate the dataframes
+    merged_df = pd.concat(dfs)
+    return merged_df
