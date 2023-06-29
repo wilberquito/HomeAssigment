b0819b90134cba32ef2db4b05e459c231dd71ea4
wilberquito <wbq.software@gmail.com>
2023-06-29 11:04:51 +0200

    Adds function to plot wordcloud of text

--- utility/plots.py
+++ utility/plots.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
+from wordcloud import WordCloud
+from wordcloud import STOPWORDS
 
 
 def confusion_matrix(y_test, y_pred, classes):
@@ -7,4 +9,29 @@ def confusion_matrix(y_test, y_pred, classes):
     disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm,
                                           display_labels=classes)
     disp.plot()
+    plt.show()
+
+
+def wordcloud(train_df):
+
+    df = train_df.copy()
+
+    # Wordcloud with positive tweets
+    positive_tweets = df['text'][df["polarity"] == 1]
+    stop_words = ["https", "co", "RT"] + list(STOPWORDS)
+    positive_wordcloud = WordCloud(max_font_size=50, max_words=50, background_color="white", stopwords = stop_words).generate(str(positive_tweets))
+    plt.figure()
+    plt.title("Positive Tweets - Wordcloud")
+    plt.imshow(positive_wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+# Wordcloud with negative tweets
+    negative_tweets = df['tweet'][df["polarity"] == 0]
+    stop_words = ["https", "co", "RT"] + list(STOPWORDS)
+    negative_wordcloud = WordCloud(max_font_size=50, max_words=50, background_color="white", stopwords = stop_words).generate(str(negative_tweets))
+    plt.figure()
+    plt.title("Negative Tweets - Wordcloud")
+    plt.imshow(negative_wordcloud, interpolation="bilinear")
+    plt.axis("off")
     plt.show()
